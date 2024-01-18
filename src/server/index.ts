4d899d7cdc510eedb4a6ec537f635ad39ee8ce15
changkhobanhoa <tientc.21it@vku.udn.vn>
2024-01-18 09:26:07 +0700

    t

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,24 +1,38 @@
-import { AddressInfo } from 'node:net';
-import * as http from 'node:http';
-import express from 'express';
-import initApi from './api';
-import initAdminApi from './admin_api';
-import errorMiddleware from './middleware/error';
 import Config from '@/config';
-import _404Middleware from './middleware/404';
-import cors from './middleware/cors';
-import { JSONMiddleware } from './middleware/JSON';
-import { stripeWebhook } from './middleware/stripe_webhook';
-import { staticFileMiddleware } from './middleware/staticFile';
-import { parseJsonMiddleware } from './middleware/parseJson';
-import { createDocs } from './docs/swagger';
+import express from 'express';
+
+import * as http from 'node:http';
+import { AddressInfo } from 'node:net';
+import path from 'node:path';
 import { Server as ServerIo } from 'socket.io';
-import { initSocket } from './websocket/sockets';
+import initAdminApi from './admin_api';
+import initApi from './api';
+import { createDocs } from './docs/swagger';
+import _404Middleware from './middleware/404';
+import { JSONMiddleware } from './middleware/JSON';
+import cors from './middleware/cors';
+import errorMiddleware from './middleware/error';
 import { logger } from './middleware/logger';
+import { parseJsonMiddleware } from './middleware/parseJson';
 import { securityHeader } from './middleware/securityHeader';
+import { staticFileMiddleware } from './middleware/staticFile';
+import { stripeWebhook } from './middleware/stripe_webhook';
+import { initSocket } from './websocket/sockets';
 
 export function createServer(): express.Express {
   const app = express();
+
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.set('views', path.join(__dirname, 'views'));
+  app.use(express.urlencoded({ extended: true }));
+
+  app.set('view engine', 'ejs');
+  app.get('/', (_, res) => {
+    // render the index template
+    res.render('index');
+  });
+
   const server = http.createServer(app);
   const io = new ServerIo(server, { cors: { origin: '*' } });
   app.set('io', io);
--- src/server/middleware/parseJson.ts
+++ src/server/middleware/parseJson.ts
@@ -1,4 +1,9 @@
 import express from 'express';
+import path from 'path';
 export function parseJsonMiddleware(app: express.Express) {
   app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.static(path.join(__dirname, 'public')));
+
+
 }
